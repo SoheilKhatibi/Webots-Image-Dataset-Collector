f1e69d6e179e3e9f526b00264475f0f2732275d3
Soheil Khatibi <soheilkhatibi1377@gmail.com>
2023-02-03 19:11:58 +0330

    Remove using webots namespace.

--- controllers/DatasetCollectionWebotsController/DatasetCollectionWebotsController.cpp
+++ controllers/DatasetCollectionWebotsController/DatasetCollectionWebotsController.cpp
@@ -1,12 +1,3 @@
-// File:          DatasetCollectionWebotsController.cpp
-// Date:
-// Description:
-// Author:
-// Modifications:
-
-// You may need to add webots include files such as
-// <webots/DistanceSensor.hpp>, <webots/Motor.hpp>, etc.
-// and/or to add some other includes
 #include <webots/Robot.hpp>
 #include <webots/Supervisor.hpp>
 #include <webots/Camera.hpp>
@@ -14,49 +5,32 @@
 // OpenCV
 #include <opencv2/opencv.hpp>
 
-// All the webots classes are defined in the "webots" namespace
-using namespace webots;
 
-// This is the main program of your controller.
-// It creates an instance of your Robot instance, launches its
-// function(s) and destroys it at the end of the execution.
-// Note that only one instance of Robot should be created in
-// a controller program.
-// The arguments of the main function can be specified by the
-// "controllerArgs" field of the Robot node
+
 int main(int argc, char **argv) {
     srand (time(NULL));
-    // create the Robot instance.
-    Supervisor *supervisor = new Supervisor();
+    // create the Supervisor instance.
+    webots::Supervisor *supervisor = new webots::Supervisor();
 
     // get the time step of the current world.
     int timeStep = (int)supervisor->getBasicTimeStep();
 
-    // do this once only
-    Node *robot_node = supervisor->getFromDef("MY_ROBOT");
+    webots::Node *robot_node = supervisor->getFromDef("MY_ROBOT");
     if (robot_node == NULL) {
         std::cerr << "No DEF MY_ROBOT node found in the current world file" << std::endl;
         exit(1);
     }
-    Field *trans_field = robot_node->getField("translation");
+    webots::Field *trans_field = robot_node->getField("translation");
 
     // Get and enable Camera
-    Camera *cam = supervisor->getCamera("Camera");
+    webots::Camera *cam = supervisor->getCamera("Camera");
     cam->enable(timeStep);
-    // You should insert a getDevice-like function in order to get the
-    // instance of a device of the robot. Something like:
-    //  Motor *motor = robot->getMotor("motorname");
-    //  DistanceSensor *ds = robot->getDistanceSensor("dsname");
-    //  ds->enable(timeStep);
 
-    // Main loop:
-    // - perform simulation steps until Webots is stopping the controller
     int simulationTime = 0;
     while (supervisor->step(timeStep) != -1) {
         simulationTime += timeStep;
         std::cout << "simulation time: " << simulationTime << std::endl;
 
-        // this is done repeatedly
         const double *values = trans_field->getSFVec3f();
         std::cout << "MY_ROBOT is at position: " << values[0] << ' '
                   << values[1] << ' ' << values[2] << std::endl;
@@ -77,7 +51,7 @@ int main(int argc, char **argv) {
         cv::waitKey(0);
 
 
-        // reset robot position and physics
+        // Reposition robot
         double randx, randy;
         // double randrot;
         randx = ((double)rand()) / RAND_MAX * 9 - 4.5;
@@ -86,19 +60,11 @@ int main(int argc, char **argv) {
 
         const double INITIAL[3] = {randx, randy, 0.279};
         trans_field->setSFVec3f(INITIAL);
+
+        // reset robot physics
         robot_node->resetPhysics();
 
-        // Read the sensors:
-        // Enter here functions to read sensor data, like:
-        //  double val = ds->getValue();
-
-        // Process sensor data here.
-
-        // Enter here functions to send actuator commands, like:
-        //  motor->setPosition(10.0);
     };
-
-    // Enter here exit cleanup code.
 
     delete supervisor;
     return 0;
