51ea47a406526ccdb2e9801c0e8da0eeeba78c33
Soheil Khatibi <soheilkhatibi1377@gmail.com>
2023-02-03 15:33:17 +0330

    Randomly reposition robot in the field.

--- controllers/DatasetCollectionWebotsController/DatasetCollectionWebotsController.cpp
+++ controllers/DatasetCollectionWebotsController/DatasetCollectionWebotsController.cpp
@@ -21,11 +21,20 @@ using namespace webots;
 // The arguments of the main function can be specified by the
 // "controllerArgs" field of the Robot node
 int main(int argc, char **argv) {
+    srand (time(NULL));
     // create the Robot instance.
     Supervisor *supervisor = new Supervisor();
 
     // get the time step of the current world.
     int timeStep = (int)supervisor->getBasicTimeStep();
+
+    // do this once only
+    Node *robot_node = supervisor->getFromDef("MY_ROBOT");
+    if (robot_node == NULL) {
+        std::cerr << "No DEF MY_ROBOT node found in the current world file" << std::endl;
+        exit(1);
+    }
+    Field *trans_field = robot_node->getField("translation");
 
     // You should insert a getDevice-like function in order to get the
     // instance of a device of the robot. Something like:
@@ -35,7 +44,28 @@ int main(int argc, char **argv) {
 
     // Main loop:
     // - perform simulation steps until Webots is stopping the controller
+    int simulationTime = 0;
     while (supervisor->step(timeStep) != -1) {
+        simulationTime += timeStep;
+        std::cout << "simulation time: " << simulationTime << std::endl;
+
+        // this is done repeatedly
+        const double *values = trans_field->getSFVec3f();
+        std::cout << "MY_ROBOT is at position: " << values[0] << ' '
+                  << values[1] << ' ' << values[2] << std::endl;
+
+
+        // reset robot position and physics
+        double randx, randy;
+        // double randrot;
+        randx = ((double)rand()) / RAND_MAX * 9 - 4.5;
+        randy = ((double)rand()) / RAND_MAX * 6 - 3;
+        // randrot = ((double)rand()) / RAND_MAX * 6.283185;
+
+        const double INITIAL[3] = {randx, randy, 0.279};
+        trans_field->setSFVec3f(INITIAL);
+        robot_node->resetPhysics();
+
         // Read the sensors:
         // Enter here functions to read sensor data, like:
         //  double val = ds->getValue();
