07f62c7937a57531350a67cb56e8177ee7a590b9
Soheil Khatibi <soheilkhatibi1377@gmail.com>
2023-02-03 14:56:58 +0330

    Configure indent size in the cpp controller.

--- controllers/DatasetCollectionWebotsController/DatasetCollectionWebotsController.cpp
+++ controllers/DatasetCollectionWebotsController/DatasetCollectionWebotsController.cpp
@@ -20,33 +20,33 @@ using namespace webots;
 // The arguments of the main function can be specified by the
 // "controllerArgs" field of the Robot node
 int main(int argc, char **argv) {
-  // create the Robot instance.
-  Robot *robot = new Robot();
+    // create the Robot instance.
+    Robot *robot = new Robot();
 
-  // get the time step of the current world.
-  int timeStep = (int)robot->getBasicTimeStep();
+    // get the time step of the current world.
+    int timeStep = (int)robot->getBasicTimeStep();
 
-  // You should insert a getDevice-like function in order to get the
-  // instance of a device of the robot. Something like:
-  //  Motor *motor = robot->getMotor("motorname");
-  //  DistanceSensor *ds = robot->getDistanceSensor("dsname");
-  //  ds->enable(timeStep);
+    // You should insert a getDevice-like function in order to get the
+    // instance of a device of the robot. Something like:
+    //  Motor *motor = robot->getMotor("motorname");
+    //  DistanceSensor *ds = robot->getDistanceSensor("dsname");
+    //  ds->enable(timeStep);
 
-  // Main loop:
-  // - perform simulation steps until Webots is stopping the controller
-  while (robot->step(timeStep) != -1) {
-    // Read the sensors:
-    // Enter here functions to read sensor data, like:
-    //  double val = ds->getValue();
+    // Main loop:
+    // - perform simulation steps until Webots is stopping the controller
+    while (robot->step(timeStep) != -1) {
+        // Read the sensors:
+        // Enter here functions to read sensor data, like:
+        //  double val = ds->getValue();
 
-    // Process sensor data here.
+        // Process sensor data here.
 
-    // Enter here functions to send actuator commands, like:
-    //  motor->setPosition(10.0);
-  };
+        // Enter here functions to send actuator commands, like:
+        //  motor->setPosition(10.0);
+    };
 
-  // Enter here exit cleanup code.
+    // Enter here exit cleanup code.
 
-  delete robot;
-  return 0;
+    delete robot;
+    return 0;
 }
